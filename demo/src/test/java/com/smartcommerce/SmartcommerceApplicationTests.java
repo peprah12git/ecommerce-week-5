07185a36fc25eec2fab3d1d18267634cf31ae7a9
Emmanuel Peprah Mensah <e.mensah@amalitech.com>
2026-02-04 18:29:22 +0000

    first commit

---
+++ demo/src/test/java/com/smartcommerce/SmartcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smartcommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartcommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
